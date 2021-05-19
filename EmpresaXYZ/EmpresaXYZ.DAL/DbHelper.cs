16ca2e0c00486b3d09d8aa7d68b7e5b6b879bfe6
figueiredosoftware <figueiredosoftware@gmail.com>
2021-05-18 22:30:27 -0300

    commit dia 18/05/2021

---
+++ EmpresaXYZ/EmpresaXYZ.DAL/DbHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+
+namespace EmpresaXYZ.DAL
+{
+    internal class DbHelper
+    {
+        //String de Conexão
+        public static string Conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Alessandro\Projetos\Projetos Versionados\App3CamadasComTestesUnitario\EmpresaXYZ\EmpresaXYZ.Models\EmpresaXYZ.mdf';Integrated Security=True";
+
+        //Abre a conexão se necessário
+        private static void AbrirConexaoSeNecessario(DbCommand cmd)
+        {
+            if (cmd.Connection.State != System.Data.ConnectionState.Open)
+            {
+                cmd.Connection.Open();
+            }
+        }
+
+        //Metodo que retorna um Command
+        public static SqlCommand ObterCommand(string sql)
+        {
+            var cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = new SqlConnection(Conexao);
+            return cmd;
+        }
+
+
+    }
+}
---
+++ EmpresaXYZ/EmpresaXYZ.Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmpresaXYZ.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
+
